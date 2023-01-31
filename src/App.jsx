44ed88162f2dd464096ae053a1755c3287c8e9f2
Nada Adel <nadaadel24657@gmail.com>
2023-01-31 18:53:29 +0200

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import './style.scss'
 
 const App = () => {
-  return <Register />;
+  return <Login />;
 };
 
 export default App;
